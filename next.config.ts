419ed1014a1d24b26d07e0cbdd23925ea4944ce9
NlengDominique <luqnleng5@gmail.com>
2025-12-19 10:37:53 +0100

    add endpoints rewrite

--- next.config.ts
+++ next.config.ts
@@ -24,14 +24,40 @@ const nextConfig: NextConfig = {
         ignoreDuringBuilds: true,
     },
 
-    // async rewrites() {
-    //     return [
-    //         {
-    //             source: "/api/:path*",
-    //             destination: "https://irelis-backend.onrender.com/:path*",
-    //         },
-    //     ];
-    // },
+    async rewrites() {
+        return [
+            {
+                source: "/auth/otp/check-mail",
+                destination: "http://api-irelis.us-east-2.elasticbeanstalk.com/auth/otp/check-mail",
+            },
+            {
+                source: "/auth/otp/request",
+                destination: "http://api-irelis.us-east-2.elasticbeanstalk.com/auth/otp/request",
+            },
+            {
+                source: "/api/v1/jobs/published",
+                destination: "http://api-irelis.us-east-2.elasticbeanstalk.com/api/v1/jobs/published",
+            },
+            {
+                source: "/auth/otp/resend",
+                destination: "http://api-irelis.us-east-2.elasticbeanstalk.com/auth/otp/resend",
+            },
+            {
+                source: "/auth/otp/verify",
+                destination: "http://api-irelis.us-east-2.elasticbeanstalk.com/auth/otp/verify",
+            },
+            {
+                source: "/auth/otp/user",
+                destination: "http://api-irelis.us-east-2.elasticbeanstalk.com/auth/otp/user",
+            },
+            {
+                source: "/auth/otp/refresh",
+                destination: "http://api-irelis.us-east-2.elasticbeanstalk.com/auth/otp/refresh",
+            },
+
+
+        ];
+    }
 };
 
 export default nextConfig;
